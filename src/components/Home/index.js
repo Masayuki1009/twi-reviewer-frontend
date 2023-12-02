f2a0c1a5f49e5f406b1bc52b436772078aeb6a70
Masayuki1009 <smasayukii1009@gmail.com>
2023-12-03 07:52:51 +0900

    created search form in Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Container, Box } from "@mui/material";
+import SearchForm from "../SearchForm";
+// 他のコンポーネントのインポートもここで行う
 
 export const Home = () => {
-  return <h1>Home page!</h1>;
+  return (
+    // Boxコンポーネントで全体をラップし、背景色をグレーに設定
+    <Box sx={{ backgroundColor: "#f5f5f5" }}>
+      {/* Containerのスタイルをここで調整する */}
+      <Container maxWidth="sm" sx={{ my: 4, py: 2 }}>
+        <SearchForm />
+        {/* 他のコンポーネントや要素があれば、ここに追加します */}
+      </Container>
+    </Box>
+  );
 };
